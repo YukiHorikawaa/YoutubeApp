d3bb80aa877ca5ad68ad7cf790c5c439944be4bf
YukiHorikawaa <mandorinyuki@icloud.com>
2022-04-04 09:21:57 +0900

    errar あり

--- PythonConnect/tag/urls.py
+++ PythonConnect/tag/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'tag'
 urlpatterns = [
-    path(r'', views.SearchTAG, name='SearchTAG'),
+    # path(r'', views.SearchTAG, name='SearchTAG'),
     # path("ajax/", views.call_write_word, name="call_write_word"),
     path("index/", views.index, name="index"),
-
+    path("nourl/", views.no_url, name="no_url_SearchTAG"),
 ]
--- PythonConnect/tag/views.py
+++ PythonConnect/tag/views.py
@@ -12,8 +12,8 @@ import json
 global output_dict
 
 
-def SearchTAG(req):
-    return render(req, 'SearchTAG.html')
+# def SearchTAG(req):
+#     return render(req, 'SearchTAG.html')
     
 
 # # def ouput(req, output_dict):
@@ -82,8 +82,78 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    params = {
-        'title' : 'Hello/Index',
-        'msg'   : 'これは、サンプルで作ってみたページでござる。'
-    }
-    return render(request, 'direct/index.html', params)
+    title_name = u"djangoを使ったPostと値の取得テスト"
+    content1 = """
+    以下に名前を入力してください
+    """
+    content_dict = {"Title" : title_name,"content1" : content1}
+    try:
+        content_dict["name"] = request.POST.get('name', None)
+    except:
+        print("error")
+    return render(request, 'direct/index.html', content_dict)
+
+
+def no_url(request):    
+    output_dict = {'title':'', 'url':'', 'tags':'', 'description':''}      
+    #POSTリクエストがあった時  
+    if request.method == 'POST':
+        print("get")
+        print("TAG")
+        searchNum = 50
+        youtube = get_youtubeData.get_youtubeData('AIzaSyDWn4f1TaQ6IwcSZBTv89a53O9FpU-xaJ8')   
+        # print(KeyEnum.Key.TAGS)
+        # write_data.pyのwrite_csv()メソッドを呼び出す。
+        # ajaxで送信したデータのうち"input_data"を指定して取得する。
+        # write_data.write_csv(request.GET.get("input_data"))
+        data = request.POST.get('input_word', None)
+        print(data)
+        #-----------------jsonでデータ取得、加工-----------------------
+        jsonmanager = get_json.get_json(data)
+        word = jsonmanager.getWord("word")
+        # print(word)
+        #-----------------指定URLに対して指定単語でデータ解析-----------------------
+        youtube.setKeyNum(word,  searchNum)
+        title = youtube.getdata(KeyEnum.KeyNum.TITLE)
+        # print(title)
+        print('---------------------------------------------------------------------------------')
+        url = youtube.getdata(KeyEnum.KeyNum.URL, UrlNum=2)
+        # print(url)
+        print('---------------------------------------------------------------------------------')
+        description = youtube.getdata(KeyEnum.KeyNum.DESCRIPTION)
+        # print(description)
+        print('---------------------------------------------------------------------------------')
+        tags, Num = youtube.getdata(KeyEnum.KeyNum.TAGS, TagNum=50, TagZip = True)
+        tags = list(tags)
+        Num = list(Num)
+        if tags[0] == None:
+            tags = tags[1:len(tags)]
+            Num = Num[1:len(Num)]
+        print(tags)
+        print("----------------------------num--------------------")
+        print(Num)
+        print('---------------------------------------------------------------------------------')
+        #-----------------それぞれのデータ取得------------------------
+        
+        output_dict = {'title':title, 'url':url, 'tags':list(tags), 'description':list(description), 'tagNum':list(Num)}
+        # print(output_dict)
+
+        # return ouput(req, output_dict)
+        # return SearchTAG(req)
+        # return render(req, 'SearchTAG.html', output_dict)
+        try:
+            encoded = json.dumps(output_dict)
+            print(HttpResponse)
+            # return render(request, 'direct/no_url_SearchTAG.html', encoded)
+            #辞書型を渡す必要がありそう
+            return render(request, 'direct/no_url_SearchTAG.html', output_dict)
+        except:
+            print(JsonResponse)
+            response = JsonResponse(output_dict)
+            response['Access-Control-Allow-Origin'] = 'http://163.43.87.213:8000'
+            #response['Access-Control-Allow-Origin'] = 'http://127.0.0.1:8000'
+            return response
+
+    else:
+        return render(request, 'direct/no_url_SearchTAG.html', output_dict)
+
