b091342fad39cffc94ced3215d5dbf20afd77e50
YukiHorikawaa <mandorinyuki@icloud.com>
2022-04-25 10:09:13 +0900

    debug

--- PythonConnect/tag/urls.py
+++ PythonConnect/tag/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path(r'', views.SearchTAG, name='SearchTAG'),
-    path("ajax/", views.call_write_word, name="call_write_word"),
+    path("tag/ajax/", views.call_write_word, name="call_write_word"),
     path("index/", views.index, name="index"),
     path("tag/", views.no_url, name="no_url_SearchTAG"),
     path("tag_out/", views.no_url, name="no_url_SearchTAG_out"),
