a6b925eff8ece1089b441fe74734e73fee8f403f
YukiHorikawaa <mandorinyuki@icloud.com>
2022-04-26 23:05:01 +0900

    STATIC_ROOT追加

--- PythonConnect/PythonConnect/settings.py
+++ PythonConnect/PythonConnect/settings.py
@@ -140,3 +140,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
